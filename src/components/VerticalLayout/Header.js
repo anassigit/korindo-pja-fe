6cd776c6f6f8fcaa1960533d8f089a93c356b4ca
vivin <vivin>
2023-06-22 10:16:16 +0000

    git-svn-id: svn://devel.svr.korindo.co.id/kor-boot-frm/korindo-pja-fe@76524 6c8e627f-5513-0410-88a3-c5d285cb66ac

--- src/components/VerticalLayout/Header.js
+++ src/components/VerticalLayout/Header.js
@@ -1,12 +1,12 @@
 import PropTypes from 'prop-types';
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 import { connect, useSelector, useDispatch } from "react-redux";
 
 // Reactstrap
 import { Form, Label, Input, Alert, FormFeedback } from "reactstrap";
 
- import ProfileMenu from "../CommonForBoth/TopbarDropdown/ProfileMenu";
+import ProfileMenu from "../CommonForBoth/TopbarDropdown/ProfileMenu";
 
 
 // actions
@@ -25,6 +25,8 @@ import {
   toggleLeftmenu,
   changeSidebarType,
 } from "../../store/actions";
+
+
 
 
 const Header = props => {
@@ -85,7 +87,7 @@ const Header = props => {
   });
 
   const error = useSelector(state => {
-    return (typeof state.Login !== "undefined" && typeof state.Login.error !== "undefined" ? state.Login.error  : "");
+    return (typeof state.Login !== "undefined" && typeof state.Login.error !== "undefined" ? state.Login.error : "");
   });
 
   // handleValidSubmit
@@ -98,7 +100,7 @@ const Header = props => {
   return (
     <React.Fragment>
       <header id="page-topbar">
-        <div style={{borderBottom: "2px solid #D9D1D1"}} className="navbar-header">
+        <div style={{ borderBottom: "2px solid #D9D1D1" }} className="navbar-header">
           <div className="d-flex">
 
             <button
@@ -109,77 +111,77 @@ const Header = props => {
               className="btn btn-sm px-3 font-size-16 header-item "
               id="vertical-menu-btn"
             >
-              <i className="fas fa-bars" style={{color:"#7BAE40"}}/>
+              <i className="fas fa-bars" style={{ color: "#7BAE40" }} />
             </button>
 
             {/* <form id="reloginForm" className="form-horizontal" style={{display: "none", marginTop: '300px', marginLeft: '500px'}}> */}
-            <Form 
-                onSubmit={(e) => {
-                  e.preventDefault();
-                  validation.handleSubmit();
-                  return false;
-                }} id="reloginForm" className="form-horizontal modal" style={{backgroundColor: 'rgb(159 159 159)',  border: '2px solid rgb(85 110 230)', margin:'auto', padding: '20px', marginLeft: '0%', width: '100%', height : '100%'}}> 
-                <div style={{backgroundColor: '#fefefe',  border: '2px solid rgb(0 0 0 / 22%)', margin:'auto', padding: '50px',  width: '450px', height : '480px'}}>
-                  <div className="modal-header">
-                          <h5 className="modal-title mt-0">
-                            Re - Login
-                          </h5>
-                  </div>
-                  <div className="modal-body">
-                        {error ? <Alert color="danger">{error}</Alert> : null}
-                        <div className="mb-3">
-                          <Label className="form-label">Nik</Label>
-                          <Input
-                            name="nik"
-                            className="form-control"
-                            placeholder="Enter Nik"
-                            type="text"
-                            onChange={validation.handleChange}
-                            onBlur={validation.handleBlur}
-                            value={validation.values.nik || ""}
-                            invalid={
-                              validation.touched.nik && validation.errors.nik ? true : false
-                            }
-                          />
-                          {validation.touched.nik && validation.errors.nik ? (
-                            <FormFeedback type="invalid">{validation.errors.nik}</FormFeedback>
-                          ) : null}
-                        </div>
-
-                        <div className="mb-3">
-                          <Label className="form-label">Password</Label>
-                          <Input
-                            name="password"
-                            value={validation.values.password || ""}
-                            type="password"
-                            placeholder="Enter Password"
-                            onChange={validation.handleChange}
-                            onBlur={validation.handleBlur}
-                            invalid={
-                              validation.touched.password && validation.errors.password ? true : false
-                            }
-                          />
-                          {validation.touched.password && validation.errors.password ? (
-                            <FormFeedback type="invalid">{validation.errors.password}</FormFeedback>
-                          ) : null}
-                        </div>
-                        <br/>
-                        <div className="mt-3 d-grid">
-                          <button
-                            className="btn btn-primary btn-block"
-                            type="submit"
-                          >
-                            Log In
-                          </button>
-                        </div>
-                    </div>
+            <Form
+              onSubmit={(e) => {
+                e.preventDefault();
+                validation.handleSubmit();
+                return false;
+              }} id="reloginForm" className="form-horizontal modal" style={{ backgroundColor: 'rgb(159 159 159)', border: '2px solid rgb(85 110 230)', margin: 'auto', padding: '20px', marginLeft: '0%', width: '100%', height: '100%' }}>
+              <div style={{ backgroundColor: '#fefefe', border: '2px solid rgb(0 0 0 / 22%)', margin: 'auto', padding: '50px', width: '450px', height: '480px' }}>
+                <div className="modal-header">
+                  <h5 className="modal-title mt-0">
+                    Re - Login
+                  </h5>
                 </div>
+                <div className="modal-body">
+                  {error ? <Alert color="danger">{error}</Alert> : null}
+                  <div className="mb-3">
+                    <Label className="form-label">Nik</Label>
+                    <Input
+                      name="nik"
+                      className="form-control"
+                      placeholder="Enter Nik"
+                      type="text"
+                      onChange={validation.handleChange}
+                      onBlur={validation.handleBlur}
+                      value={validation.values.nik || ""}
+                      invalid={
+                        validation.touched.nik && validation.errors.nik ? true : false
+                      }
+                    />
+                    {validation.touched.nik && validation.errors.nik ? (
+                      <FormFeedback type="invalid">{validation.errors.nik}</FormFeedback>
+                    ) : null}
+                  </div>
+
+                  <div className="mb-3">
+                    <Label className="form-label">Password</Label>
+                    <Input
+                      name="password"
+                      value={validation.values.password || ""}
+                      type="password"
+                      placeholder="Enter Password"
+                      onChange={validation.handleChange}
+                      onBlur={validation.handleBlur}
+                      invalid={
+                        validation.touched.password && validation.errors.password ? true : false
+                      }
+                    />
+                    {validation.touched.password && validation.errors.password ? (
+                      <FormFeedback type="invalid">{validation.errors.password}</FormFeedback>
+                    ) : null}
+                  </div>
+                  <br />
+                  <div className="mt-3 d-grid">
+                    <button
+                      className="btn btn-primary btn-block"
+                      type="submit"
+                    >
+                      Log In
+                    </button>
+                  </div>
+                </div>
+              </div>
             </Form>
 
-          
+
           </div>
           <div className="d-flex">
-                {/* <form style={{boxShadow: '1px 1px 2px #fffefe'}} className="p-3">
+            {/* <form style={{boxShadow: '1px 1px 2px #fffefe'}} className="p-3">
                         <div className="form-group m-0">
                           <div className="input-group">
                             <input
@@ -298,20 +300,20 @@ const Header = props => {
             </Dropdown> */}
 
             <select className="selectpicker" data-width="fit">
-                <option data-content='<span class="flag-icon flag-icon-us"></span> English'>English</option>
-              <option  data-content='<span class="flag-icon flag-icon-mx"></span> Español'>Español</option>
+              <option data-content='<span class="flag-icon flag-icon-us"></span> English'>English</option>
+              <option data-content='<span class="flag-icon flag-icon-mx"></span> Español'>Español</option>
             </select>
 
             <div className="dropdown d-none d-lg-inline-block ms-2">
               <button
                 type="button"
                 onClick={() => {
-                  window.location.reload(); 
+                  window.location.reload();
                 }}
                 className="btn header-item noti-icon "
                 data-toggle="reload"
               >
-                <i className="bx bx-sync" style={{color:"#7BAE40"}} />
+                <i className="bx bx-sync" style={{ color: "#7BAE40" }} />
               </button>
             </div>
 
@@ -332,7 +334,7 @@ const Header = props => {
             {/* <NotificationDropdown /> */}
             <ProfileMenu />
 
-            
+
           </div>
         </div>
       </header>
