1ee804bc23868560a7d13ec798a64e8fb3a17fcb
sigit <sigit>
2023-07-20 04:59:07 +0000

    git-svn-id: svn://devel.svr.korindo.co.id/kor-boot-frm/korindo-pja-fe@76866 6c8e627f-5513-0410-88a3-c5d285cb66ac

--- src/pages/AppSetting/Setting.js
+++ src/pages/AppSetting/Setting.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import {
+    Button,
     Card,
     CardBody,
     CardHeader,
@@ -19,20 +20,20 @@ const Setting = () => {
     const [appSettingMsg, setappSettingMsg] = useState("")
     const [appInstructionsPage, setAppInstructionsPage] = useState(true)
 
-    const [radioValue1, setRadioValue1] = useState("option1");
-    const [radioValue2, setRadioValue2] = useState("option1");
-    const [radioValue3, setRadioValue3] = useState("option1");
-  
+    const [radioValue1, setRadioValue1] = useState(null);
+    const [radioValue2, setRadioValue2] = useState(null);
+    const [radioValue3, setRadioValue3] = useState(null);
+
     const handleRadioChange1 = (event) => {
-      setRadioValue1(event.target.value);
+        setRadioValue1(event.target.value);
     };
-  
+
     const handleRadioChange2 = (event) => {
-      setRadioValue2(event.target.value);
+        setRadioValue2(event.target.value);
     };
-  
+
     const handleRadioChange3 = (event) => {
-      setRadioValue3(event.target.value);
+        setRadioValue3(event.target.value);
     };
 
     return (
@@ -43,8 +44,6 @@ const Setting = () => {
 
                         <Row>
                             <Col>
-
-
                                 <Card>
                                     <CardHeader style={{ borderRadius: "15px 15px 0 0" }}>
                                         <strong>General Settings</strong>
@@ -52,88 +51,133 @@ const Setting = () => {
 
                                     <CardBody>
                                         <React.Fragment>
-                                            <Row>
-                                                <Col md="12" lg="4">
-                                                    <Row>
-                                                        <b>Instruction Display Settings 1</b>
+                                            <Row className="mb-2">
+                                                <Col md="12" lg="4" >
+                                                    <Row className="mb-2">
+                                                        <b>Instruction Display Settings</b>
                                                     </Row>
                                                     <Row>
                                                         <label>
                                                             <Input
                                                                 type="radio"
-                                                                name="option1"
-                                                                value="option1"
-                                                                checked={radioValue1 === "option1"}
+                                                                name="ins_display_setting"
+                                                                value="0"
+                                                                checked={radioValue1 === "0"}
                                                                 onChange={handleRadioChange1}
                                                             />
-                                                            Option 1
+                                                            <span> </span>Show Your Own Instructions
                                                         </label>
                                                         <label>
                                                             <Input
                                                                 type="radio"
-                                                                name="option1"
-                                                                value="option2"
-                                                                checked={radioValue1 === "option2"}
+                                                                name="ins_display_setting"
+                                                                value="1"
+                                                                checked={radioValue1 === "1"}
                                                                 onChange={handleRadioChange1}
                                                             />
-                                                            Option 2
+                                                            <span> </span>Show All Instructions
                                                         </label>
                                                     </Row>
                                                 </Col>
                                                 <Col md="12" lg="4">
-                                                    <Row>
-                                                        <b>Instruction Display Settings 2</b>
+                                                    <Row className="mb-2">
+                                                        <b>Notification Settings</b>
                                                     </Row>
                                                     <Row>
                                                         <label>
                                                             <Input
                                                                 type="radio"
-                                                                name="option2"
-                                                                value="option1"
-                                                                checked={radioValue2 === "option1"}
+                                                                name="ins_notice_setting"
+                                                                value="0"
+                                                                checked={radioValue2 === "0"}
                                                                 onChange={handleRadioChange2}
                                                             />
-                                                            Option 1
+                                                            <span> </span>Send Notifications
                                                         </label>
                                                         <label>
                                                             <Input
                                                                 type="radio"
-                                                                name="option2"
-                                                                value="option2"
-                                                                checked={radioValue2 === "option2"}
+                                                                name="ins_notice_setting"
+                                                                value="1"
+                                                                checked={radioValue2 === "1"}
                                                                 onChange={handleRadioChange2}
                                                             />
-                                                            Option 2
+                                                            <span> </span>Don&rsquo;t Send Notifications
                                                         </label>
                                                     </Row>
                                                 </Col>
                                                 <Col md="12" lg="4">
-                                                    <Row>
-                                                        <b>Instruction Display Settings 3</b>
+                                                    <Row className="mb-2">
+                                                        <b>Notification Settings 2</b>
                                                     </Row>
                                                     <Row>
                                                         <label>
                                                             <Input
                                                                 type="radio"
-                                                                name="option3"
-                                                                value="option1"
-                                                                checked={radioValue3 === "option1"}
+                                                                name="ins_notice2_setting"
+                                                                value="0"
+                                                                checked={radioValue3 === "0"}
                                                                 onChange={handleRadioChange3}
                                                             />
-                                                            Option 1
+                                                            <span> </span>WhatsApp
                                                         </label>
                                                         <label>
                                                             <Input
                                                                 type="radio"
-                                                                name="option3"
-                                                                value="option2"
-                                                                checked={radioValue3 === "option2"}
+                                                                name="ins_notice2_setting"
+                                                                value="1"
+                                                                checked={radioValue3 === "1"}
                                                                 onChange={handleRadioChange3}
                                                             />
-                                                            Option 2
+                                                            <span> </span>Email
+                                                        </label>
+                                                        <label>
+                                                            <Input
+                                                                type="radio"
+                                                                name="ins_notice2_setting"
+                                                                value="2"
+                                                                checked={radioValue3 === "2"}
+                                                                onChange={handleRadioChange3}
+                                                            />
+                                                            <span> </span>Both
                                                         </label>
                                                     </Row>
                                                 </Col>
+                                            </Row>
+                                            <Row className="mb-2">
+                                                <Col className="d-flex justify-content-end">
+                                                    <div className="col-12 col-lg-2">
+                                                        <button className="btn btn-primary w-100">Apply</button>
+                                                    </div>
+                                                </Col>
+                                            </Row>
+                                        </React.Fragment>
+                                    </CardBody>
+                                </Card>
+                                
+                                <Card>
+                                    <CardHeader style={{ borderRadius: "15px 15px 0 0" }}>
+                                        <strong>General Settings</strong>
+                                    </CardHeader>
+
+                                    <CardBody>
+                                        <React.Fragment>
+                                            <Row className="mb-2">
+                                               
+                                            </Row>
+                                        </React.Fragment>
+                                    </CardBody>
+                                </Card>
+                                
+                                <Card>
+                                    <CardHeader style={{ borderRadius: "15px 15px 0 0" }}>
+                                        <strong>General Settings</strong>
+                                    </CardHeader>
+
+                                    <CardBody>
+                                        <React.Fragment>
+                                            <Row className="mb-2">
+                                                
                                             </Row>
                                         </React.Fragment>
                                     </CardBody>
