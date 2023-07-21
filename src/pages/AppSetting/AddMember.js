fb30147b7096bc3c12d6a9119cdc07c05a4e7e90
sigit <sigit>
2023-07-21 07:27:14 +0000

    git-svn-id: svn://devel.svr.korindo.co.id/kor-boot-frm/korindo-pja-fe@76898 6c8e627f-5513-0410-88a3-c5d285cb66ac

---
+++ src/pages/AppSetting/AddMember.js
@@ -0,0 +1,324 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+    Button,
+    Card,
+    CardBody,
+    CardHeader,
+    Col,
+    Container,
+    Input,
+    Row,
+    UncontrolledTooltip
+} from "reactstrap";
+import RootPageCustom from '../../common/RootPageCustom';
+import '../../config';
+import { getMembersData, resetMessage } from "store/appSetting/actions";
+import TableCustom2 from "common/TableCustom2";
+
+
+const Setting = () => {
+
+    const dispatch = useDispatch();
+    const [appSettingMsg, setAppSettingMsg] = useState("")
+    const [appSettingPage, setAppSettingPage] = useState(true)
+
+    const [radioValue1, setRadioValue1] = useState(null);
+    const [radioValue2, setRadioValue2] = useState(null);
+    const [radioValue3, setRadioValue3] = useState(null);
+    const [generalSettingArr, setGeneralSettingArr] = useState({ langType: "eng", instructionDisplay: "", notification: "", notification2: "" })
+
+    const [appMembersTabelSearch, setAppMembersTabelSearch] = useState({
+        page: 1, limit: 10, offset: 0, sort: "id", order: "desc", search: {
+            any: "", langType: "eng"
+        }
+    });
+
+    useEffect(() => {
+        dispatch(resetMessage());
+        dispatch(getMembersData({ langType: "eng", instructionDisplay: "", notification: "", notification2: "" }));
+    }, [dispatch])
+    const appMembersData = useSelector(state => {
+        return state.settingReducer.respGetMembers;
+    });
+
+    const appMembersp01Tabel = [
+        {
+            dataField: "name",
+            text: "Name",
+            sort: true,
+            align: "left",
+            headerStyle: { textAlign: 'center' },
+        },
+        {
+            dataField: "id",
+            text: "Email",
+            sort: true,
+            align: "left",
+            headerStyle: { textAlign: 'center' },
+        },
+        {
+            dataField: "hp",
+            text: "HP",
+            sort: true,
+            align: "left",
+            headerStyle: { textAlign: 'center' },
+        },
+        {
+            dataField: "rname",
+            text: "Rank",
+            sort: true,
+            align: "left",
+            headerStyle: { textAlign: 'center' },
+        },
+        {
+            dataField: "pname",
+            text: "Permission",
+            sort: true,
+            align: "left",
+            headerStyle: { textAlign: 'center' },
+        },
+        {
+            dataField: "bgcolor",
+            text: "Background Color",
+            sort: true,
+            align: "left",
+            headerStyle: { textAlign: 'center' },
+        },
+        {
+            dataField: "edit",
+            isDummyField: true,
+            text: "Edit",
+            headerStyle: { textAlign: 'center' },
+            formatter: (cellContent, data) => (
+                <>
+                    <div style={{ justifyContent: 'center' }} className="d-flex gap-3">
+                        <i className="mdi mdi-pencil font-size-18  text-primary" id="edittooltip" onClick={() => app008p01PreEdit(data)} />
+                        <UncontrolledTooltip placement="top" target="edittooltip">
+                            Ubah
+                        </UncontrolledTooltip>
+                    </div>
+                </>
+            ),
+        },
+        {
+            dataField: "delete",
+            isDummyField: true,
+            text: "Delete",
+            headerStyle: { textAlign: 'center' },
+            formatter: (cellContent, data) => (
+                <>
+                    <div style={{ justifyContent: 'center' }} className="d-flex gap-3">
+                        <i className="mdi mdi-delete font-size-18 text-danger" id="deletetooltip" onClick={() => app008p01Delete(data)} />
+                        <UncontrolledTooltip placement="top" target="deletetooltip">
+                            Hapus
+                        </UncontrolledTooltip>
+                    </div>
+                </>
+            ),
+        },
+    ]
+
+    useEffect(() => {
+        if (appMembersData.status == "0") {
+            setAppSettingMsg(appMembersData)
+        }
+    }, [appMembersData])
+
+
+    useEffect(() => {
+        dispatch(getMembersData({ langType: "eng", instructionDisplay: "", notification: "", notification2: "" }));
+    }, [appMembersTabelSearch])
+
+    const handleRadioChange1 = (event) => {
+        setRadioValue1(event.target.value);
+    };
+
+    const handleRadioChange2 = (event) => {
+        setRadioValue2(event.target.value);
+    };
+
+    const handleRadioChange3 = (event) => {
+        setRadioValue3(event.target.value);
+    };
+
+    const appSettingPreEdit = (e) => {
+        null
+    }
+
+    return (
+        <RootPageCustom msgStateGet={appSettingMsg} msgStateSet={setAppSettingMsg}
+            componentJsx={
+                <>
+                    <Container style={{ display: appSettingPage ? 'block' : 'none' }} fluid={true}>
+
+                        <Row>
+                            <Col>
+                                <Card>
+                                    <CardHeader style={{ borderRadius: "15px 15px 0 0" }}>
+                                        <strong>General Settings</strong>
+                                    </CardHeader>
+
+                                    <CardBody>
+                                        <React.Fragment>
+                                            <Row className="mb-2">
+                                                <Col md="12" lg="4" >
+                                                    <Row className="mb-2">
+                                                        <b>Instruction Display Settings</b>
+                                                    </Row>
+                                                    <Row>
+                                                        <label>
+                                                            <Input
+                                                                type="radio"
+                                                                name="ins_display_setting"
+                                                                value="0"
+                                                                checked={radioValue1 === "0"}
+                                                                onChange={handleRadioChange1}
+                                                            />
+                                                            <span> </span>Show Your Own Members
+                                                        </label>
+                                                        <label>
+                                                            <Input
+                                                                type="radio"
+                                                                name="ins_display_setting"
+                                                                value="1"
+                                                                checked={radioValue1 === "1"}
+                                                                onChange={handleRadioChange1}
+                                                            />
+                                                            <span> </span>Show All Members
+                                                        </label>
+                                                    </Row>
+                                                </Col>
+                                                <Col md="12" lg="4">
+                                                    <Row className="mb-2">
+                                                        <b>Notification Settings</b>
+                                                    </Row>
+                                                    <Row>
+                                                        <label>
+                                                            <Input
+                                                                type="radio"
+                                                                name="ins_notice_setting"
+                                                                value="0"
+                                                                checked={radioValue2 === "0"}
+                                                                onChange={handleRadioChange2}
+                                                            />
+                                                            <span> </span>Send Notifications
+                                                        </label>
+                                                        <label>
+                                                            <Input
+                                                                type="radio"
+                                                                name="ins_notice_setting"
+                                                                value="1"
+                                                                checked={radioValue2 === "1"}
+                                                                onChange={handleRadioChange2}
+                                                            />
+                                                            <span> </span>Don&rsquo;t Send Notifications
+                                                        </label>
+                                                    </Row>
+                                                </Col>
+                                                <Col md="12" lg="4">
+                                                    <Row className="mb-2">
+                                                        <b>Notification Settings 2</b>
+                                                    </Row>
+                                                    <Row>
+                                                        <label>
+                                                            <Input
+                                                                type="radio"
+                                                                name="ins_notice2_setting"
+                                                                value="0"
+                                                                checked={radioValue3 === "0"}
+                                                                onChange={handleRadioChange3}
+                                                            />
+                                                            <span> </span>WhatsApp
+                                                        </label>
+                                                        <label>
+                                                            <Input
+                                                                type="radio"
+                                                                name="ins_notice2_setting"
+                                                                value="1"
+                                                                checked={radioValue3 === "1"}
+                                                                onChange={handleRadioChange3}
+                                                            />
+                                                            <span> </span>Email
+                                                        </label>
+                                                        <label>
+                                                            <Input
+                                                                type="radio"
+                                                                name="ins_notice2_setting"
+                                                                value="2"
+                                                                checked={radioValue3 === "2"}
+                                                                onChange={handleRadioChange3}
+                                                            />
+                                                            <span> </span>Both
+                                                        </label>
+                                                    </Row>
+                                                </Col>
+                                            </Row>
+                                            <Row className="mb-2">
+                                                <Col className="d-flex justify-content-end">
+                                                    <div className="col-12 col-lg-2">
+                                                        <button className="btn btn-primary w-100">Apply</button>
+                                                    </div>
+                                                </Col>
+                                            </Row>
+                                        </React.Fragment>
+                                    </CardBody>
+                                </Card>
+
+                                <Row className="my-3 mt-5">
+                                    <Col className="d-flex justify-content-end">
+                                        <div className="col-12 col-lg-2">
+                                            <button className="btn btn-primary w-100">Add Member</button>
+                                        </div>
+                                    </Col>
+                                </Row>
+
+                                <Card>
+                                    <CardHeader style={{ borderRadius: "15px 15px 0 0" }}>
+                                        <strong>Members</strong>
+                                    </CardHeader>
+
+                                    <CardBody>
+                                        <React.Fragment>
+                                            <Row className="mb-2">
+                                                <TableCustom2
+                                                    keyField={"id"}
+                                                    columns={appMembersp01Tabel}
+                                                    redukResponse={appMembersData}
+                                                    appdata={appMembersData?.data?.memberList != null ? appMembersData?.data?.memberList : []}
+                                                    appdataTotal={appMembersData?.data?.memberList?.length != null ? appMembersData?.data?.memberList?.length : 0}
+                                                    searchSet={setAppMembersTabelSearch}
+                                                    searchGet={appMembersTabelSearch}
+                                                    redukCall={getMembersData}
+                                                />
+                                            </Row>
+                                        </React.Fragment>
+                                    </CardBody>
+                                </Card>
+
+                                <Card>
+                                    <CardHeader style={{ borderRadius: "15px 15px 0 0" }}>
+                                        <strong>General Settings</strong>
+                                    </CardHeader>
+
+                                    <CardBody>
+                                        <React.Fragment>
+                                            <Row className="mb-2">
+
+                                            </Row>
+                                        </React.Fragment>
+                                    </CardBody>
+                                </Card>
+                            </Col>
+                        </Row>
+                    </Container>
+
+
+                </>
+            }
+        />
+    );
+
+
+}
+export default Setting
