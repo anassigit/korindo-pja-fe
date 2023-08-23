a34d3b7e742eeb65552b617f57a47fb941f92598
sigit <sigit>
2023-08-23 04:20:32 +0000

    modal repair
    git-svn-id: svn://devel.svr.korindo.co.id/kor-boot-frm/korindo-pja-fe@77647 6c8e627f-5513-0410-88a3-c5d285cb66ac

--- src/components/Common/MsgModal.js
+++ src/components/Common/MsgModal.js
@@ -1,32 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Modal, ModalHeader, ModalBody, ModalFooter, Button, Spinner } from 'reactstrap';
-import '../../assets/scss/custom/modal/modal.css'
-
-const MsgModal = ({ modal, toggle, message, isHidden }) => {
+const MsgModal = ({ modal, toggle, message }) => {
     return (
         <Modal isOpen={modal} toggle={toggle} backdrop="static">
-            <ModalHeader toggle={toggle}>{isHidden == null ? 'Message' : 'Loading'}</ModalHeader>
+            <ModalHeader toggle={toggle}>Message</ModalHeader>
             <ModalBody>
-                {message == null ? (
-                    <div className='d-flex justify-content-center'>
-                        <Spinner
-                            animation="grow"
-                            style={{
-                                width: '25px',
-                                height: '25px',
-                                display: 'block',
-                                left: '50%',
-                                top: '50%',
-                            }}
-                            color="danger"
-                        />
-                    </div>
-                ) : (
+                {
                     message
-                )}
+                }
             </ModalBody>
-            <ModalFooter hidden={isHidden == null ? false : isHidden}>
+            <ModalFooter>
                 <Button color="danger" onClick={toggle}>
                     Close
                 </Button>
@@ -39,7 +23,6 @@ MsgModal.propTypes = {
     modal: PropTypes.any,
     toggle: PropTypes.any,
     message: PropTypes.any,
-    isHidden: PropTypes.any,
 };
 
 export default MsgModal;
