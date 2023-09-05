a57921460862e2706cc80e611635e2ddf0d46197
sigit <sigit>
2023-09-05 04:24:52 +0000

    update frontend
    git-svn-id: svn://devel.svr.korindo.co.id/kor-boot-frm/korindo-pja-fe@77868 6c8e627f-5513-0410-88a3-c5d285cb66ac

--- src/pages/AppFileManagement/EnterMonthlyData.js
+++ src/pages/AppFileManagement/EnterMonthlyData.js
@@ -16,7 +16,9 @@ import {
   UncontrolledDropdown,
   CardTitle,
   CardGroup,
-  Input
+  Input,
+  CardDeck,
+  Button
 } from "reactstrap"
 import { ContextMenu, MenuItem, ContextMenuTrigger } from "react-contextmenu";
 import { getSelectFile, deleteFileFolder, resetMessage, getSearch, respGetDownloadCheckFile, downloadCheckFile } from "../../store/appFileManagement/actions"
@@ -55,343 +57,12 @@ const EnterMonthlyData = (props) => {
   const dispatch = useDispatch();
   const [monthlyDataPage, setMonthlyDataPage] = useState(true)
   const [monthlyDataMsg, setMonthlyDataMsg] = useState("")
-  const [idParent, setIdParent] = useState(0)
-  const [idChild, setIdChild] = useState(-1)
-  const [idToggle, setIdToggle] = useState("")
-  const [idToggleUpload, setIdToggleUpload] = useState("")
-  const [idToggleCreate, setIdToggleCreate] = useState("")
-  const [nmToggle, setNmToggle] = useState("")
-  const [nmToggleExt, setNmToggleExt] = useState("")
-  const [renameModal, setRenameModal] = useState(false)
-  const [uploadModal, setUploadModal] = useState(false)
-  const [createModal, setCreateModal] = useState(false)
-  const [moveModal, setMoveModal] = useState(false)
-  const [confirmModalDelete, setConfirmModalDelete] = useState(false)
-  const [confirmModalDownload, setConfirmModalDownload] = useState(false)
-  const [isYes, setIsYes] = useState(false)
-  const [downloadFlag, setDownloadFlag] = useState(false)
-  const [idFolderTemp, setIdFolderTemp] = useState()
-  const [idParentTemp, setIdParentTemp] = useState()
-  const [fNum, setFnum] = useState(0)
-  const [pNum, setPnum] = useState(0)
-  const [fNem, setFnem] = useState("")
-  const [fName, setFName] = useState("")
-  const [tempIdDel, setTempIdDel] = useState()
-  const [currFolder, setCurrFolder] = useState()
-  const [isTypeFolder, setIsTypeFolder] = useState()
-  const [idNowLoc, setIdNowLoc] = useState(0)
-  const [typeRename, setTypeRename] = useState("")
-  const [downloadMsg, setDownloadMsg] = useState(false)
-  const [downloadMsgModal, setDownloadMsgModal] = useState(false)
-  const [downloadContentModal, setDownloadContentModal] = useState("")
-  const [successClose, setSuccessClose] = useState(false)
-  const [appFileManagementData, setAppFileManagementData] = useState(null);
 
-  // folder left-right click
-  const [isContextMenuOpen, setContextMenuOpen] = useState(false);
-  const [isDropdownMenuOpen, setDropdownMenuOpen] = useState(false);
-
-  // file left-right click
-  const [isContextMenuOpen2, setContextMenuOpen2] = useState(false);
-  const [isDropdownMenuOpen2, setDropdownMenuOpen2] = useState(false);
-
-  const contextMenuRef = useRef(null);
-
-  const toggleRenameModal = (idT, nmT, tpT) => {
-
-    setIdNowLoc(currFolder)
-    setIdToggle(idT)
-    if (tpT === "FILE") {
-      var realNm = nmT.split('.').slice(0, -1).join('.')
-      setNmToggle(realNm)
-      var extNm = nmT.split('.').pop();
-      setNmToggleExt(extNm)
-      setTypeRename(tpT)
-    } else {
-      setNmToggle(nmT)
-      setTypeRename(tpT)
-    }
-    setRenameModal(!renameModal)
-  }
-
-  const toggleUploadModal = () => {
-    debugger
-    setIdNowLoc(currFolder)
-    setUploadModal(!uploadModal)
-    setIdToggleUpload(idChild)
-  }
-
-  const toggleCreateModal = () => {
-
-    setIdNowLoc(currFolder)
-    setCreateModal(!createModal)
-    setIdToggleCreate(idChild)
-  }
-
-  const toggleMoveModal = (Fid, Pid) => {
-    debugger
-    setFnum(Fid)
-    setPnum(Pid)
-    setFName(fNem)
-    setIdNowLoc(currFolder)
-    setSuccessClose(false)
-    setMoveModal(!moveModal)
-
-  }
-
-  const confirmToggleDownload = (e, typeFolder) => {
-
-    if (e) {
-      setTempIdDel(e)
-      setIsTypeFolder(typeFolder)
-    }
-    setConfirmModalDownload(!confirmModalDownload)
-  }
-
-  const confirmToggleDelete = (e, typeFolder) => {
-    debugger
-    if (e) {
-      setTempIdDel(e)
-      setIsTypeFolder(typeFolder)
-    }
-    setConfirmModalDelete(!confirmModalDelete)
-  }
-
-  useEffect(() => {
-
-    const storedData = localStorage.getItem('appFileManagementData');
-    let parsedData = null
-
-    if (storedData) {
-      parsedData = JSON.parse(storedData);
-      setCurrFolder(parsedData);
-    }
-
-    dispatch(resetMessage());
-
-    if (storedData !== null) {
-      dispatch(getSelectFile({ 'folder_num': storedData }))
-    } else {
-      dispatch(getSelectFile())
-    }
-    dispatch(getSearch({
-      "search": ""
-    }))
-  }, [])
-
-
-
-  /* KUMPULAN USE SELECTOR */
-
-  const [realFileList, setRealFileList] = useState()
-  const [realFilePath, setRealFilePath] = useState()
-
-  const getFileSelect = useSelector(state => {
-    return state.fileManagementReducer.respGetSelect;
-  })
-
-  const getSearchFile = useSelector(state => {
-    return state.fileManagementReducer.respGetSearchFile;
-  })
-
-  const downloadRespMsg = useSelector(state => {
-    return state.fileManagementReducer.respGetDownloadCheck;
-  })
-
-  useEffect(() => {
-
-  }, [realFileList])
-
-  useEffect(() => {
-
-
-    if (getFileSelect) {
-      setRealFileList(getFileSelect?.data?.childList)
-      setRealFilePath(getFileSelect?.data?.path);
-    }
-  }, [getFileSelect])
-
-  useEffect(() => {
-
-
-    if (getSearchFile?.data !== null) {
-      setRealFileList(getSearchFile?.data?.searchList)
-    } else if (getSearchFile?.data === null) {
-      setRealFileList(getFileSelect?.data?.childList)
-    }
-  }, [getSearchFile])
-
-
-  const msgDeleteFile = useSelector(state => {
-    return state.fileManagementReducer.msgDelete;
-  })
-
-
-  const fileManagementCloseAlert = () => {
-    setMonthlyDataMsg("")
-  }
-
-  const getInsideFolder = (e, f, n) => {
-
-    setCurrFolder(e)
-    localStorage.setItem('appFileManagementData', JSON.stringify(e));
-    dispatch(getSelectFile({ 'folder_num': e }))
-    setIdFolderTemp(e)
-    setIdChild(e)
-    setIdParentTemp(f)
-    setIdParent(f)
-    setFnem(n)
-  }
-
-  useEffect(() => {
-
-    if (isYes) {
-      let num = null
-      num = tempIdDel
-      num.toString()
-
-      dispatch(deleteFileFolder(
-        {
-          'file_num': num
-        }
-      ))
-
-      if (msgDeleteFile?.status == "1") {
-        dispatch(getSelectFile({ 'folder_num': currFolder }))
-        setIsYes(!isYes)
-      }
-    }
-  }, [isYes, msgDeleteFile])
-
-
-  const [numTemp, setNumTemp] = useState()
-  const [fileNmTemp, setFileNmTemp] = useState()
-
-  const downloadCheckFile1 = (num, fileNm) => {
-    debugger
-    setNumTemp(num)
-    setFileNmTemp(fileNm)
-    setDownloadFlag(true)
-  }
-
-  const downloadCheckFile2 = (num, fileNm) => {
-    debugger
-    setDownloadFlag(true)
-
-  };
-
-  const toggleMsgModal = () => {
-    setDownloadMsgModal(!downloadMsgModal);
-
-    if (downloadMsg.status === "0") {
-      downloadCheckFile2
-      handleEffect();
-
-    }
-
-  };
-
-  useEffect(() => {
-    debugger
-    if (downloadFlag) {
-
-      const indexed_array = {
-        file_num: numTemp,
-        file_nm: fileNmTemp
-      };
-      dispatch(downloadCheckFile(indexed_array))
-      setDownloadFlag(false)
-
-    }
-  }, [downloadFlag, fileNmTemp, numTemp])
-
-  const downloadFolderFile = async () => {
-    try {
-
-      const indexed_array = {
-        file_num: numTemp,
-        file_nm: fileNmTemp
-      };
-      dispatch(downloadFileFolder(indexed_array))
-    } catch (error) {
-      console.log(error)
-    }
-  };
-
-  useEffect(() => {
-    debugger
-    if (downloadRespMsg.status === "0") {
-      setDownloadMsg(downloadRespMsg);
-      toggleMsgModal()
-
-    } else if (downloadRespMsg.status === "1") {
-      downloadFolderFile()
-    }
-    setDownloadContentModal(downloadRespMsg.message)
-    setDownloadMsg("");
-
-  }, [downloadRespMsg])
 
   const handleSearchChange = (e) => {
     dispatch(getSearch({ "search": e.target.value }))
   }
 
-
-  // folder left-right click
-
-  const [contextMenuPosition, setContextMenuPosition] = useState({ x: 0, y: 0 });
-
-
-  const handleContextMenuOpen = (e) => {
-    debugger
-    e.preventDefault();
-
-    const xPos = e.clientX;
-    const yPos = e.clientY;
-
-    setContextMenuOpen(true);
-    setDropdownMenuOpen(false);
-    setContextMenuPosition({ x: xPos, y: yPos });
-  };
-
-  const handleDropdownMenuToggle = () => {
-    debugger
-    setDropdownMenuOpen(true);
-    setContextMenuOpen(false);
-    setContextMenuOpen2(false);
-  };
-
-  // file left-right click
-
-  const [contextMenuPosition2, setContextMenuPosition2] = useState({ x: 0, y: 0 });
-
-
-  const handleContextMenuOpen2 = (e) => {
-    debugger
-    e.preventDefault();
-
-    const xPos2 = e.clientX;
-    const yPos2 = e.clientY;
-
-    setContextMenuOpen2(true);
-    setDropdownMenuOpen2(false);
-    setContextMenuPosition2({ x: xPos2, y: yPos2 });
-  };
-
-  const handleDropdownMenuToggle2 = () => {
-    debugger
-    setDropdownMenuOpen2(true);
-    setContextMenuOpen2(false);
-    setContextMenuOpen(false);
-  };
-
-
-  useEffect(() => {
-    debugger
-    if (!isDropdownMenuOpen2) {
-      setContextMenuOpen2(true);
-    }
-  }, [isContextMenuOpen2])
 
   const dateChanger = (name, selectedDate) => {
     // let convertedDate = selectedDate.toISOString().substr(0, 7);
@@ -417,6 +88,19 @@ const EnterMonthlyData = (props) => {
     //     }));
     // }
   };
+
+
+  const cardsData = [
+    { id: 1, content: 'Short Card 1' },
+    { id: 2, content: 'Short Card 2' },
+    { id: 3, content: 'Long Card 1' },
+    { id: 4, content: 'Short Card 3' },
+    { id: 5, content: 'Long Card 2' },
+    { id: 6, content: 'Short Card 4' },
+    { id: 7, content: 'Long Card 3' },
+    { id: 8, content: 'Short Card 5' },
+    { id: 9, content: 'Long Card 4' },
+  ];
 
 
   return (
@@ -445,9 +129,9 @@ const EnterMonthlyData = (props) => {
                       </Row>
                     </Col>
                     <Col sm="12">
-                      <div className="d-flex justify-content-end">
+                      <div className="d-flex justify-content-end" style={{ marginRight: "12px" }}>
                         <Col md='1' className="d-flex justify-content-end">
-                          <Row className="mb-1" style={{ width: "80%", marginRight: "-24%" }} >
+                          <Row className="mb-1">
                             <Input
                               name="year"
                               type="select"
@@ -459,19 +143,26 @@ const EnterMonthlyData = (props) => {
                             //   value={editInstructionsValidInput.values.status}
                             //   invalid={editInstructionsValidInput.touched.status && editInstructionsValidInput.errors.status}
                             >
-                            
+
                             </Input>
                           </Row>
                         </Col>
                         &nbsp;
                         <Col md='1' className="d-flex justify-content-end ">
-                          <Row className="mb-1" style={{ width: "80%" }}>
-                            <DatePicker
-                              className="form-control text-center"
-                              dateFormat="yyyy-MM-dd"
-                            // onChange={}
-                            // selected={editInstructionsValidInput.values.insDate ? new Date(editInstructionsValidInput.values.insDate) : null}
-                            />
+                          <Row className="mb-1">
+                            <Input
+                              name="year"
+                              type="select"
+                              placeholder="YYYY"
+                            //   onChange={(e) => {
+                            //     editInstructionsValidInput.handleChange(e)
+                            //   }}
+
+                            //   value={editInstructionsValidInput.values.status}
+                            //   invalid={editInstructionsValidInput.touched.status && editInstructionsValidInput.errors.status}
+                            >
+
+                            </Input>
                           </Row>
                         </Col>
                       </div>
@@ -482,44 +173,33 @@ const EnterMonthlyData = (props) => {
                 <p />
                 <p />
                 <Row className="mb-2">
-                  <Col sm='12'>
-                    <CardGroup>
-                      <Card className="col-lg-3 col-md-6" style={{ flex: "1", marginRight: "2%" }}>
+                  {cardsData.map((card, index) => (
+                    <Col sm={12} md={6} lg={3} key={index}>
+                      <Card className="mb-2">
                         <CardBody>
-                          Short Card
+                          <Row className="text-center justify-content-center">
+                            <span style={{fontSize: "62px", color: "#7BAE40", opacity:"0.75"}} className="mdi mdi-file-check-outline"></span>
+                            <span style={{fontSize: "24px", marginTop: "-2%"}}>test</span>
+                          </Row>
+                          <Row>
+                            <Col>
+                              <span style={{fontSize: "16px", fontWeight: "bold", whiteSpace: "nowrap" }}>Company Name</span>
+                            </Col>
+                            <Col className="text-end">
+                              <Button
+                                style={{ paddingTop: "2px", paddingBottom: "2px" }}
+                              >
+                                Upload
+                              </Button>
+                            </Col>
+                          </Row>
                         </CardBody>
                       </Card>
-                      <Card className="col-lg-3 col-md-6" style={{ flex: "1", marginRight: "2%" }}>
-                        <CardBody>
-                          Short Card
-                        </CardBody>
-                      </Card>
-                      <Card className="col-lg-3 col-md-6" style={{ flex: "1", marginRight: "2%" }}>
-                        <CardBody>
-                          Long Card
-                        </CardBody>
-                      </Card>
-                      <Card className="col-lg-3 col-md-6" style={{ flex: "1" }}>
-                        <CardBody>
-                          Long Card
-                        </CardBody>
-                      </Card>
-                    </CardGroup>
-                  </Col>
+                    </Col>
+                  ))}
                 </Row>
 
-                <p />
-                <p />
-                <Row className="mb-1 col-sm-10"><h6>{"   "}{props.t("Folders")}</h6></Row>
-                <p />
-                <Row className="mb-2">
-                  <Col sm="12">
-                    <Row>
 
-
-                    </Row>
-                  </Col>
-                </Row>
               </Col>
             </Row>
           </Container>
