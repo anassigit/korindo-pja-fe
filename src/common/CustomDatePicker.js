00ddcee98b57e430e576c4e695dd8f6d2b328e35
sigit <sigit>
2023-09-07 04:39:39 +0000

    fix custom date picker
    git-svn-id: svn://devel.svr.korindo.co.id/kor-boot-frm/korindo-pja-fe@77923 6c8e627f-5513-0410-88a3-c5d285cb66ac

--- src/common/CustomDatePicker.js
+++ src/common/CustomDatePicker.js
@@ -1,15 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import DatePicker from 'react-datepicker';
 import PropTypes from 'prop-types';
 import 'react-datepicker/dist/react-datepicker.css';
 
 function CustomDatePicker({ selected, onChange, onClear }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const inputRef = useRef(null);
+
   const handleDateChange = (date) => {
     onChange(date);
+    setIsOpen(false); // Close the date picker after selecting a date
   };
 
   const handleClear = () => {
     onClear();
+    inputRef.current.blur(); // Remove focus from the input
+  };
+
+  const handleInputClick = () => {
+    if (!isOpen) {
+      setIsOpen(true);
+    }
+  };
+
+  const handleInputKeyDown = (e) => {
+    if (e.key === 'Escape') {
+      setIsOpen(false);
+    }
   };
 
   return (
@@ -22,22 +39,27 @@ function CustomDatePicker({ selected, onChange, onClear }) {
         customInput={
           <div className="date-picker-input">
             <input
+              ref={inputRef}
               className="form-control"
               placeholderText="YYYY-MM"
               value={selected ? selected.toISOString().slice(0, 7) : ''}
+              onClick={handleInputClick}
+              onKeyDown={handleInputKeyDown}
             />
             {selected && (
               <button
                 type="button"
                 className="clear-button"
                 onClick={handleClear}
-                style={{ position: 'absolute', right: '5px', top: '7px', border:"0", backgroundColor:"transparent", color:"#495057", opacity:'0.5' }}
+                style={{ position: 'absolute', right: '5px', top: '7px', border: "0", backgroundColor: "transparent", color: "#495057", opacity: '0.5' }}
               >
                 <span className="mdi mdi-close"></span>
               </button>
             )}
           </div>
         }
+        open={isOpen}
+        onCalendarClose={() => setIsOpen(false)}
       />
     </div>
   );
